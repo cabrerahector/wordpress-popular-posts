6e2f37cd3a26e075749213810f0eeb755a732cb9
Ikki24 <Ikki24@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-18 14:52:37 +0000

    Author / plugin links updated
    git-svn-id: http://plugins.svn.wordpress.org/wordpress-popular-posts/tags/1.2@207603 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-popular-posts.php
+++ wordpress-popular-posts.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Wordpress Popular Posts
-Plugin URI: http://rauru.com/wordpress-popular-posts
+Plugin URI: http://wordpress.org/extend/plugins/wordpress-popular-posts/
 Description: Retrieves the most active entries of your blog and displays them on a list. Use it as a widget or place it in your templates using  <strong>&lt;?php get_mostpopular('Title', number of posts); ?&gt;</strong>
 Version: 1.2
 Author: H&eacute;ctor Cabrera
-Author URI: http://rauru.com/
+Author URI: http://wordpress.org/extend/plugins/wordpress-popular-posts/
 */
 
 // widget core
