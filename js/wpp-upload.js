a97ca1e9e6f813853ca5cb1c67439b9dfdbaf23d
Héctor Cabrera <hcabrerab@gmail.com>
2014-06-02 07:46:17 -0430

    Moves click event catching to the top of the script

--- js/wpp-upload.js
+++ js/wpp-upload.js
@@ -1,10 +1,8 @@
 jQuery(document).ready(function($) {
 	
 	$('#upload_thumb_button').click(function(e) {
-		
-		tb_show('Upload a thumbnail', 'media-upload.php?referer=wpp_admin&type=image&TB_iframe=true&post_id=0', false);
 		e.preventDefault();
-		
+		tb_show('Upload a thumbnail', 'media-upload.php?referer=wpp_admin&type=image&TB_iframe=true&post_id=0', false);
 	});
 	
 	window.send_to_editor = function(html) {
