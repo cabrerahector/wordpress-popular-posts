de928288af94a69ff4d365621508298b44dd4c07
Héctor Cabrera <hcabrerab@gmail.com>
2014-06-04 10:18:56 -0430

    Adds check for ABSPATH

--- wordpress-popular-posts.php
+++ wordpress-popular-posts.php
@@ -29,7 +29,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-if ( basename($_SERVER['SCRIPT_NAME']) == basename(__FILE__) )
+if ( !defined('ABSPATH') )
 	exit('Please do not load this file directly.');
 
 /**
