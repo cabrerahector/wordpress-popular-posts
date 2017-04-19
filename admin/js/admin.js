bcaec8e332957f01e26658513ffab70848b5eba5
Héctor Cabrera <hcabrerab@gmail.com>
2017-04-19 14:47:47 -0400

    Removes unused code from admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -66,36 +66,6 @@
 
         });
 
-        // ADS
-        if ( $('#wpp_donate').is(':visible') ) {
-
-            $.ajax({
-                type: "GET",
-                url: "http://cabrerahector.com/ads/get.php",
-                timeout: 5000,
-                dataType: "jsonp",
-                success: function(results){
-                    if ( !$.isEmptyObject(results) ) {
-                        $("#wpp_advertisement").html( results.ad );
-
-                        setTimeout(function(){
-                            // Ad blocker detected :(
-                            if ( "none" == $("#wpp_advertisement img").css('display') ) {
-                                $("#wpp_advertisement").html('<h3 style="margin-top:0; font-size: 1.7em; text-align:center; line-height: 1em;">An <em>awesome</em> ad would be here...</h3><p style="font-size:1.1em; text-align:center;">... <em>if you weren\'t using an <strong>ad blocker</strong></em> :(</p><p style="font-size:0.8em; line-height: 1.4em;">Showing ads help us developers offer our services for free to everyone, so please consider disabling your ad blocker for this page.</p><p style="font-size:0.8em; line-height: 1.4em;">It won\'t be an annoying one, I promise :)</p>').show();
-                            }
-
-                            $("#wpp_advertisement").show();
-                        }, 250);
-                    }
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown){
-                    if ( window.console && window.console.log )
-                        window.console.log( 'Could not retrieve the ad: ' + textStatus );
-                }
-            });
-
-        }
-
         // STATISTICS TABS
         $("#wpp-stats-tabs a").click(function(e){
             var activeTab = $(this).attr("rel");
