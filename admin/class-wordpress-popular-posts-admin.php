08a38c992aaaf755aa388164008ff23857d53dca
Héctor Cabrera <hcabrerab@gmail.com>
2018-06-15 11:16:23 -0400

    Bumps minimum required PHP version to 5.3

--- admin/class-wordpress-popular-posts-admin.php
+++ admin/class-wordpress-popular-posts-admin.php
@@ -1292,7 +1292,7 @@ class WPP_Admin {
 
         global $wp_version;
 
-        $php_min_version = '5.2';
+        $php_min_version = '5.3';
         $wp_min_version = '4.7';
         $php_current_version = phpversion();
         $errors = array();
