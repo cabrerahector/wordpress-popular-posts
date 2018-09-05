174177f3c8f8e61b5e5fd05a133ed20ff076e334
Héctor Cabrera <hcabrerab@gmail.com>
2018-09-05 15:01:25 -0400

    Store views in persistent object cache

--- includes/class-wordpress-popular-posts-rest-controller.php
+++ includes/class-wordpress-popular-posts-rest-controller.php
@@ -211,34 +211,131 @@ class WP_REST_Popular_Posts_Controller extends WP_REST_Controller {
         if ( has_action( 'wpp_pre_update_views' ) )
             do_action( 'wpp_pre_update_views', $post_ID, $views );
 
+        $result1 = $result2 = false;
+
         $exec_time = 0;
         $start = WPP_helper::microtime_float();
 
-        // Update all-time table
-        $result1 = $wpdb->query( $wpdb->prepare(
-            "INSERT INTO {$table}data
-            (postid, day, last_viewed, pageviews) VALUES (%d, %s, %s, %d)
-            ON DUPLICATE KEY UPDATE pageviews = pageviews + %d, last_viewed = %s;",
-            $post_ID,
-            $now,
-            $now,
-            $views,
-            $views,
-            $now
-        ));
+        // Store views data in persistent object cache
+        if (
+            wp_using_ext_object_cache()
+            && defined( 'WPP_CACHE_VIEWS' )
+            && WPP_CACHE_VIEWS
+        ) {
 
-        // Update range (summary) table
-        $result2 = $wpdb->query( $wpdb->prepare(
-            "INSERT INTO {$table}summary
-            (postid, pageviews, view_date, view_datetime) VALUES (%d, %d, %s, %s)
-            ON DUPLICATE KEY UPDATE pageviews = pageviews + %d, view_datetime = %s;",
-            $post_ID,
-            $views,
-            $curdate,
-            $now,
-            $views,
-            $now
-        ));
+            $now = new DateTime( WPP_Helper::now() );
+            $timestamp = $now->getTimestamp();
+
+            if ( ! $wpp_cache = wp_cache_get( '_wpp_cache', 'transient' ) ) {
+
+                $wpp_cache = array(
+                    'last_updated' => $now->format('Y-m-d H:i:s'),
+                    'data' => array(
+                        $post_ID => array(
+                            $timestamp => 1
+                        )
+                    )
+                );
+
+            } else {
+
+                if ( ! isset( $wpp_cache['data'][$post_ID] ) ) {
+                    $wpp_cache['data'][$post_ID][$timestamp] = 1;
+                } else {
+
+                    if ( isset($wpp_cache['data'][$post_ID][$timestamp]) ) {
+                        $wpp_cache['data'][$post_ID][$timestamp] += 1;
+                    } else {
+                        $wpp_cache['data'][$post_ID][$timestamp] = 1;
+                    }
+
+                }
+
+            }
+
+            // Update cache
+            wp_cache_set( '_wpp_cache', $wpp_cache, 'transient', 0 );
+
+            // How long has it been since the last time we saved to the database?
+            $last_update = $now->diff( new DateTime($wpp_cache['last_updated']) );
+            $diff_in_minutes = $last_update->days * 24 * 60;
+            $diff_in_minutes += $last_update->h * 60;
+            $diff_in_minutes += $last_update->i;
+
+            // It's been more than 5 minutes, save everything to DB
+            if ( $diff_in_minutes > 2 ) {
+
+                $query_data = "INSERT INTO {$table}data (`postid`,`day`,`last_viewed`,`pageviews`) VALUES ";
+                $query_summary = "INSERT INTO {$table}summary (`postid`,`pageviews`,`view_date`,`view_datetime`) VALUES ";
+
+                foreach( $wpp_cache['data'] as $pid => $data ) {
+
+                    $views_count = 0;
+
+                    foreach( $data as $ts => $cached_views ){
+                        $views_count += $cached_views;
+
+                        $query_summary .= $wpdb->prepare( "(%d,%d,%s,%s),", array(
+                            $pid,
+                            $cached_views,
+                            date("Y-m-d", $ts),
+                            date("Y-m-d H:i:s", $ts)
+                        ));
+                    }
+
+                    $query_data .= $wpdb->prepare( "(%d,%s,%s,%s),", array(
+                        $pid,
+                        $now->format('Y-m-d H:i:s'),
+                        $now->format('Y-m-d H:i:s'),
+                        $views_count
+                    ));
+
+                }
+
+                $query_data = rtrim( $query_data, ",") . " ON DUPLICATE KEY UPDATE pageviews=pageviews+VALUES(pageviews),last_viewed=VALUES(last_viewed);";
+                $query_summary = rtrim( $query_summary, ",") . ";";
+
+                // Clear cache
+                $wpp_cache['last_updated'] = $now->format('Y-m-d H:i:s');
+                $wpp_cache['data'] = array();
+                wp_cache_set( '_wpp_cache', $wpp_cache, 'transient', 0 );
+
+                // Save
+                $result1 = $wpdb->query( $query_data );
+                $result2 = $wpdb->query( $query_summary );
+
+            }
+
+        } // Live update to the DB
+        else {
+
+            // Update all-time table
+            $result1 = $wpdb->query( $wpdb->prepare(
+                "INSERT INTO {$table}data
+                (postid, day, last_viewed, pageviews) VALUES (%d, %s, %s, %d)
+                ON DUPLICATE KEY UPDATE pageviews = pageviews + %d, last_viewed = %s;",
+                $post_ID,
+                $now,
+                $now,
+                $views,
+                $views,
+                $now
+            ));
+
+            // Update range (summary) table
+            $result2 = $wpdb->query( $wpdb->prepare(
+                "INSERT INTO {$table}summary
+                (postid, pageviews, view_date, view_datetime) VALUES (%d, %d, %s, %s)
+                ON DUPLICATE KEY UPDATE pageviews = pageviews + %d, view_datetime = %s;",
+                $post_ID,
+                $views,
+                $curdate,
+                $now,
+                $views,
+                $now
+            ));
+
+        }
 
         $end = WPP_helper::microtime_float();
         $exec_time += round( $end - $start, 6 );
