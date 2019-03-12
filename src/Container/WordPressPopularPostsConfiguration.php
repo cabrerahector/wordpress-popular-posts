f74ddd2761be95752b6da1e35c6936c4eff6f8cd
Héctor Cabrera <hcabrerab@gmail.com>
2019-03-12 09:28:55 -0400

    Store data from Settings on its own Container keys

--- src/Container/WordPressPopularPostsConfiguration.php
+++ src/Container/WordPressPopularPostsConfiguration.php
@@ -13,6 +13,9 @@ class WordPressPopularPostsConfiguration implements ContainerConfigurationInterf
      */
     public function modify(Container $container)
     {
+        $container['admin_options'] = Settings::get('admin_options');
+        $container['widget_options'] = Settings::get('widget_options');
+
         $container['translate'] = $container->service(function(Container $container) {
             return new \WordPressPopularPosts\Translate();
         });
@@ -22,19 +25,19 @@ class WordPressPopularPostsConfiguration implements ContainerConfigurationInterf
         });
 
         $container['output'] = $container->service(function(Container $container) {
-            return new \WordPressPopularPosts\Output(Settings::get('widget_options'), Settings::get('admin_options'), $container['image'], $container['translate']);
+            return new \WordPressPopularPosts\Output($container['widget_options'], $container['admin_options'], $container['image'], $container['translate']);
         });
 
         $container['widget'] = $container->service(function(Container $container) {
-            return new \WordPressPopularPosts\Widget\Widget(Settings::get('widget_options'), Settings::get('admin_options'), $container['output'], $container['image']);
+            return new \WordPressPopularPosts\Widget\Widget($container['widget_options'], $container['admin_options'], $container['output'], $container['image']);
         });
 
         $container['rest'] = $container->service(function(Container $container) {
-            return new \WordPressPopularPosts\Rest\Controller(Settings::get('admin_options'), $container['translate']);
+            return new \WordPressPopularPosts\Rest\Controller($container['admin_options'], $container['translate']);
         });
 
         $container['front'] = $container->service(function(Container $container) {
-            return new \WordPressPopularPosts\Front\Front(Settings::get('admin_options'), $container['translate'], $container['output']);
+            return new \WordPressPopularPosts\Front\Front($container['admin_options'], $container['translate'], $container['output']);
         });
 
         $container['wpp'] = $container->service(function(Container $container) {
