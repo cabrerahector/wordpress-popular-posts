336db687f2c7c3435a7184acb604590c7f63a4fe
Héctor Cabrera <hcabrerab@gmail.com>
2020-06-24 17:51:59 -0400

    Updates copyright notice

--- uninstall.php
+++ uninstall.php
@@ -6,7 +6,7 @@
  * @author    Hector Cabrera <me@cabrerahector.com>
  * @license   GPL-2.0+
  * @link      https://cabrerahector.com
- * @copyright 2008-2019 Hector Cabrera
+ * @copyright 2008-2020 Hector Cabrera
  */
 
 // If uninstall is not called from WordPress, exit
