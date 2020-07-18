7a1bb84cfe06f91142e067f73e1b02d7df7a5d24
Héctor Cabrera <hcabrerab@gmail.com>
2020-07-18 10:52:40 -0400

    Removes version number from wpp.min.js

--- src/Front/Front.php
+++ src/Front/Front.php
@@ -99,7 +99,7 @@ class Front {
             $is_single = Helper::is_single();
         }
 
-        wp_register_script('wpp-js', plugin_dir_url(dirname(dirname(__FILE__))) . 'assets/js/wpp-5.2.1.min.js', [], WPP_VERSION, false);
+        wp_register_script('wpp-js', plugin_dir_url(dirname(dirname(__FILE__))) . 'assets/js/wpp.min.js', [], WPP_VERSION, false);
         $params = [
             'sampling_active' => (int) $this->config['tools']['sampling']['active'],
             'sampling_rate' => (int) $this->config['tools']['sampling']['rate'],
