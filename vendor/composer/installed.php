1593f19eef14f333adb4aadfe5d0b98e6c89e7bd
Héctor Cabrera <hcabrerab@gmail.com>
2022-06-30 10:11:30 -0400

    Updates composer's autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '951350ece2d03c669abdcd9f61f36a79fee1a35b',
+    'name' => 'cabrerahector/wordpress-popular-posts',
+  ),
+  'versions' => 
+  array (
+    'cabrerahector/wordpress-popular-posts' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '951350ece2d03c669abdcd9f61f36a79fee1a35b',
+    ),
+  ),
+);
