e72e59589d336eea8988fb1208f4b652505e1ff9
Héctor Cabrera <hcabrerab@gmail.com>
2023-12-01 10:04:11 -0400

    Admin: Fixes invisible custom field error message (closes #375)

--- src/Admin/admin-page.php
+++ src/Admin/admin-page.php
@@ -52,7 +52,7 @@ if ( isset($_POST['section']) ) {
                 $_POST['thumb_source'] == 'custom_field'
                 && ( ! isset($_POST['thumb_field']) || empty($_POST['thumb_field']) )
             ) {
-                echo '<div id="wpp-message" class="error fade"><p>' . esc_html(__('Please provide the name of your custom field.', 'wordpress-popular-posts')) . '</p></div>';
+                echo '<div class="notice notice-error"><p>' . esc_html(__('Please provide the name of your custom field.', 'wordpress-popular-posts')) . '</p></div>';
             }
             else {
                 // thumbnail settings changed, flush transients
