fe50585815dba7836f27db41f83cd5e9ee1dc9c1
Héctor Cabrera <hcabrerab@gmail.com>
2024-03-09 12:20:23 -0400

    Admin: fixes #383, default thumb reverting to stock

--- src/Admin/admin-page.php
+++ src/Admin/admin-page.php
@@ -62,7 +62,7 @@ if ( isset($_POST['section']) ) {
 
                 $this->config['tools']['thumbnail']['source'] = sanitize_text_field($_POST['thumb_source']);
                 $this->config['tools']['thumbnail']['field'] = ( ! empty($_POST['thumb_field']) ) ? sanitize_text_field($_POST['thumb_field']) : 'wpp_thumbnail';
-                $this->config['tools']['thumbnail']['default'] = ( ! empty($_POST['upload_thumb_src']) ) ? $_POST['upload_thumb_src'] : '';
+                $this->config['tools']['thumbnail']['default'] = ( ! empty($_POST['upload_thumb_src']) ) ? $_POST['upload_thumb_src'] : $this->config['tools']['thumbnail']['default'];
                 $this->config['tools']['thumbnail']['resize'] = (bool) $_POST['thumb_field_resize'];
                 $this->config['tools']['thumbnail']['lazyload'] = (bool) $_POST['thumb_lazy_load'];
 
