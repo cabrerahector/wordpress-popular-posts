37f7d6b3e224a6d2b754a443984feb61e38a2913
Héctor Cabrera <hcabrerab@gmail.com>
2024-12-03 08:53:15 -0400

    Bumps version to 7.2.0

--- wordpress-popular-posts.php
+++ wordpress-popular-posts.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WordPress Popular Posts
  * Plugin URI:        https://wordpress.org/plugins/wordpress-popular-posts/
  * Description:       A highly customizable plugin that displays the most popular posts on your blog.
- * Version:           7.1.0
+ * Version:           7.2.0
  * Requires at least: 5.7
  * Requires PHP:      7.2
  * Author:            Hector Cabrera
@@ -31,7 +31,7 @@ if ( ! defined( 'WPINC' ) ) {
     die();
 }
 
-define('WPP_VERSION', '7.1.0');
+define('WPP_VERSION', '7.2.0');
 
 $wpp_main_plugin_file = __FILE__;
 // Load plugin bootstrap
