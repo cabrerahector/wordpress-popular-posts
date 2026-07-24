5aa06140dd418dcdb6aedead0e7070f148555df1
Héctor Cabrera <hcabrerab@gmail.com>
2026-07-24 15:04:31 -0400

    Updates dependencies

--- assets/js/blocks/block-wpp-widget.asset.php
+++ assets/js/blocks/block-wpp-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'd80898919fb8fc7daaf2');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime'), 'version' => '1dbacbe301ba3920508f');
